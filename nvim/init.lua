48f5196d0ff6980c29024804c5637d1f89cf14fd
dlguswo333 <dlguswo002@gmail.com>
2025-02-26 13:32:37 +0900

    nvim: Replace barbecue plugin with dropbar

--- nvim/init.lua
+++ nvim/init.lua
@@ -255,17 +255,18 @@ local plugins = {
   },
   -- Show symbols and structures inside the file.
   {
-    "utilyre/barbecue.nvim",
-    name = "barbecue",
-    version = false,
-    dependencies = {
-      "SmiteshP/nvim-navic",
-      "nvim-tree/nvim-web-devicons",
-    },
-    opts = {
-      show_dirname = false,
-      show_filename = false,
-    },
+    'Bekaboo/dropbar.nvim',
+    -- optional, but required for fuzzy finder support
+    -- dependencies = {
+    --   'nvim-telescope/telescope-fzf-native.nvim',
+    --   build = 'make'
+    -- },
+    config = function()
+      local dropbar_api = require('dropbar.api')
+      vim.keymap.set('n', '<Leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+      vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
+      vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+    end,
     enabled = use_lsp
   },
   -- Show function signatures and parameters while typing.
